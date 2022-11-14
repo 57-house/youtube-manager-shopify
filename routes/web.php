64629c5d8f7ab4b887366a4a41c268fe2ecedf2d
dnjiegnam <dnjiegnam@yob.fr>
2022-11-14 22:45:27 +0100

    Fix flash back

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/login', function (Request $request) {
         "1c9726d70aba1c6838b71222733b0889",
         "b86c924cbaa3e22f904eeaf4d4075d5b",
         "read_products,write_products",
-        "localhost",
+        "filemanager.americaa.de",
         new FileSessionStorage(base_path() . '/tmp/php_sessions'),
         '2022-04',
         true,
