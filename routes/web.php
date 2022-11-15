1d0472bc8bb14d713f6f8991729f825fe0ce9202
dnjiegnam <dnjiegnam@yob.fr>
2022-11-15 11:14:56 +0100

    Fix flash back

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,18 @@ Route::get('/login', function (Request $request) {
 
 
 Route::get('/callback', function (Request $request) {
+
+    Shopify\Context::initialize(
+        "1c9726d70aba1c6838b71222733b0889",
+        "b86c924cbaa3e22f904eeaf4d4075d5b",
+        "read_products,write_products",
+        "flash-sales.americaa.de",
+        new FileSessionStorage(base_path() . '/tmp/php_sessions'),
+        '2022-04',
+        true,
+        false,
+    );
+    
     dd(Shopify\Auth\OAuth::callback([
         "cookie" => md5($_GET["shop"]),
     ], $_GET));
